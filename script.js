3ffe7f0c7f443544069c2fcfba9f4898e2214d7b
unknown <satyavssatya12@gmail.com>
2023-07-09 10:41:30 +0530

    project final

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    var location = document.getElementById('location').value;
+    var jobRole = document.getElementById('jobRole').value;
+  
+  });
+  
